7aadf405badf60657cdf92e6b95492f7f2e292f5
Gaitho57 <michaelgaitho10@gmail.com>
2024-03-28 06:39:59 +0000

    update

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,40 +1,82 @@
 #!/usr/bin/python3
-"""
-Script that reads stdin line by line and computes metrics
-"""
+""" Script that reads stdin line by line and computes metrics
 
+    Input format:
+        <IP Address> - [<date>] "GET /projects/260 HTTP/1.1"
+            <status code> <file size>
+        (if the format is not this one, the line must be skipped)
 
-import re
+    format: <status code>: <number>
+"""
 import sys
+import re
 
 
-# Initialize variables to store statistics
-size = 0
-num_lines = 0
-status_codes = {"200": 0, "301": 0, "400": 0, "401": 0, "403": 0, "404": 0,
-                "405": 0, "500": 0}
-pattern = (
-    r'^(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}) - \[(\d{4}-\d{2}-\d{2} .*)\]'
-    r' "GET /projects/260 HTTP/1\.1"\s(200|301|[4-5]\d{2}) (\d+)'
-)
+def print_status(total_size, status_codes):
+    """ Print Status Function """
+    print(f"File size: {total_size}")
+    for code, count in status_codes.items():
+        if count > 0:
+            print(f"{code}: {count}")
 
 
-try:
-    for input_str in sys.stdin:
-        re_match = re.match(pattern, input_str)
-        if re_match:
-            num_lines += 1
-            size += int(re_match.group(4))
-            status_codes[re_match.group(3)] += 1
-            if num_lines % 10 == 0:
-                print("File size: {}".format(size))
-                for key, value in sorted(status_codes.items()):
-                    if value > 0:
-                        print("{}: {}".format(key, value))
-except KeyboardInterrupt:
-    pass
-finally:
-    print("File size: {}".format(size))
-    for key, value in sorted(status_codes.items()):
-        if value > 0:
-            print("{}: {}".format(key, value))
+def match_input(input_line):
+    """ Check if input line match """
+    data = {'status_code': 0, 'file_size': 0}
+
+    pattern = re.compile(r'\b^[0-9]+.[0-9]+.[0-9]+.[0-9]+ - '
+                         r'\[[0-9]+-[0-9]+-[0-9]+ '
+                         r'[0-9]+:[0-9]+:[0-9]+.[0-9]+\] '
+                         r'"GET \/projects\/260 HTTP\/1.1" '
+                         r'(200|301|400|401|403|404|405|500) ([0-9]+)$\b')
+
+    match = pattern.search(input_line)
+    if not match:
+        return (None)
+
+    data['status_code'] = int(match.group(1))
+    data['file_size'] = int(match.group(2))
+    return (data)
+
+
+def main():
+    """ Main Function """
+    total_size = 0
+    status_codes = {
+                200: 0,
+                301: 0,
+                400: 0,
+                401: 0,
+                403: 0,
+                404: 0,
+                405: 0,
+                500: 0
+            }
+    counter = 0
+
+    try:
+        for line in sys.stdin:
+            data = match_input(line)
+            counter += 1
+
+            if not data:
+                continue
+
+            status_code = data['status_code']
+
+            if status_code not in status_codes:
+                continue
+
+            total_size += data['file_size']
+            status_codes[status_code] += 1
+
+            if counter % 10 == 0:
+                print_status(total_size, status_codes)
+        print_status(total_size, status_codes)
+    except KeyboardInterrupt:
+        print_status(total_size, status_codes)
+        raise
+
+
+if __name__ == "__main__":
+    main()
